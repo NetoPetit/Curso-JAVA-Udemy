c086e9148df8930e84723b69b3ca55c9eb8f357a
netopetit <cidao_petit5@hotmail.com>
2026-01-10 13:25:47 -0300

    collection set e list

--- src/classe/AreaCircTeste.java
+++ src/classe/AreaCircTeste.java
@@ -9,7 +9,7 @@ public class AreaCircTeste {
 		//a1.pi = 10;
 		//System.out.println(a1.area());
 		
-		AreaCirc a2 = new AreaCirc(5);
+		//AreaCirc a2 = new AreaCirc(5);
 		//a2.pi = 5;
 		
 		//AreaCirc.pi = 3.1415;
